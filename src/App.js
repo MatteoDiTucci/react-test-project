fbb349e0f1eac48648c0edc07ba5297716e20e1c
matteoditucci <matteo.ditucci@gmail.com>
2021-02-27 11:51:27 +0100

    Change main page title to check continuous deployment workflow

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Yo, learn React
         </a>
       </header>
     </div>
