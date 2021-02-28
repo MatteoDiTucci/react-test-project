dd88598025e7389b534a853fa30d64fe36fdc5e0
matteoditucci <matteo.ditucci@gmail.com>
2021-02-28 09:53:10 +0100

    Dummy commit to test CI/CD

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Yo, learn React
+          Yo, Mateo learns React
         </a>
       </header>
     </div>
